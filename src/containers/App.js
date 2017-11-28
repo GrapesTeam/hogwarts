13063478dabe25fa10a40d054493dc48f794f637
Edward <edward.zhang.xw@gmail.com>
2017-11-29 01:12:38 +0800

    add react-router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Signin from './Signin';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Route path="/signin" component={Signin} />
       </div>
     );
   }
